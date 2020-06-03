cc8081525b0fb824e81e30c41fcd90301c05eaf0
Bulat <sadikovba@gmail.com>
2020-06-03 11:38:12 +0300

    Add coordinates

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 let game = {
   ctx: null,
+  platform: null,
+  ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
   sprites: {
     background: null,
     ball: null,
     platform: null,
+    block: null,
   },
 
   init() {
@@ -13,17 +19,29 @@ let game = {
 
   preload(callback) {
     let loaded = 0;
-    let required = Object.keys(this.sprites).length;
+    const required = Object.keys(this.sprites).length;
+    const onImageLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    };
 
     for (let key in this.sprites) {
       this.sprites[key] = new Image();
       this.sprites[key].src = `img/${key}.png`;
-      this.sprites[key].addEventListener('load', () => {
-        ++loaded;
-        if (loaded >= required) {
-          callback();
-        }
-      });
+      this.sprites[key].addEventListener("load", onImageLoad);
+    }
+  },
+
+  create() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          x: 64 * col + 65,
+          y: 24 * row + 35,
+        });
+      }
     }
   },
 
@@ -35,16 +53,46 @@ let game = {
 
   render() {
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.sprites.platform, 0, 0);
+    this.ctx.drawImage(
+      this.sprites.ball,
+      0,
+      0,
+      this.ball.width,
+      this.ball.height,
+      this.ball.x,
+      this.ball.y,
+      this.ball.width,
+      this.ball.height
+    );
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    this.renderBlocks();
+  },
+
+  renderBlocks() {
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
   },
 
   start() {
     this.init();
     this.preload(() => {
-      this.run()
+      this.create();
+      this.run();
     });
   },
+};
+
+game.ball = {
+  x: 320,
+  y: 280,
+  width: 20,
+  height: 20,
+};
+
+game.platform = {
+  x: 280,
+  y: 300,
 };
 
 window.addEventListener("load", () => {
